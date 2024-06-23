dfe2bf928592d9b3021888f285d7ffbb01e7a776
ArkAow <tom.charpentier@outlook.fr>
2024-06-23 18:55:54 +0200

    updated world and player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,30 @@
+@icon("res://assets/icons/player-icon.png")
 class_name Player extends CharacterBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+const WALK_SPEED: float = 3.0
+const ACCELERATION: float = 30.0
+const FRICTION: float = 30.0
+var input_direction: Vector2
+var direction: Vector3
 
+func _physics_process(delta):
+	input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	direction = (Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	player_animation()
+	player_movement(delta)
+	move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func player_animation():
+	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Run/blend_position", input_direction)
+	if velocity == Vector3.ZERO:
+		anim_state.travel("Idle")
+	else:
+		anim_state.travel("Run")
+
+func player_movement(delta):
+	if direction: velocity = velocity.move_toward(direction * WALK_SPEED , delta * ACCELERATION)
+	else: velocity = velocity.move_toward(Vector3(0,0,0), delta * FRICTION)
